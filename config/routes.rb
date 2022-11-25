ef287751ab418021a72871d0717f0b5243c80560
Tushar <singhtushar348@gmail.com>
2022-11-25 20:22:50 +0530

    Add routes for login, signup, logout api endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :sessions, only: [:create]
+  resources :registrations, only: [:create]
+  resources :users
+  delete :logout, to: "sessions#logout"
+  get :logged_in, to: "sessions#logged_in"
 end
