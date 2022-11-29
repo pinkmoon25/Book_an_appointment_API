67dc9c9efe638a467bdb2f9794946d0ba1ea3733
Tushar <singhtushar348@gmail.com>
2022-11-29 13:59:13 +0530

    Include actioncontroller for cookies handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,3 @@
-class ApplicationController < ActionController::Base
-  skip_before_action :verify_authenticity_token
+class ApplicationController < ActionController::API
+  include ActionController::Cookies
 end
