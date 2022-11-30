f6cacc5c2bd5e8f1203360fcc945c8adebe7e5fb
Tushar <singhtushar348@gmail.com>
2022-11-30 14:16:22 +0530

    Create route for mentors data endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,9 @@ Rails.application.routes.draw do
   resources :users
   delete :logout, to: "sessions#logout"
   get :logged_in, to: "sessions#logged_in"
+  namespace :api do
+    namespace :v1 do
+      resources :mentors, only: [:index]
+    end
+  end
 end
