cd5e237911feb091b3dd99937941862967b6a14a
Tushar <singhtushar348@gmail.com>
2022-12-01 17:08:37 +0530

    Add create action for reservations

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,2 +1,14 @@
 class Api::V1::ReservationsController < ApplicationController
+  def create
+    @reservation = Reservation.new(date: params[:date], user_id: params[:user_id],
+                                   mentor_id: params[:mentor_id])
+    if @reservation.save
+      render json: @reservation.to_json(include: %i[mentor])
+    else
+      render json: {
+        status: 404,
+        error: @reservation.errors.full_messages
+      }
+    end
+  end
 end
