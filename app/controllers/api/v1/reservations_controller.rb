013fc1945b06d15a8ea5802f1fbf29e64fd4f769
Tushar <singhtushar348@gmail.com>
2022-12-07 15:07:59 +0530

    Add subject params

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::ReservationsController < ApplicationController
   def create
     @reservation = Reservation.new(date: params[:date], user_id: params[:user_id],
-                                   mentor_id: params[:mentor_id])
+                                   mentor_id: params[:mentor_id], subject: params[:subject])
     if @reservation.save
       render json: @reservation.to_json(include: %i[mentor])
     else
@@ -10,5 +10,12 @@ class Api::V1::ReservationsController < ApplicationController
         error: @reservation.errors.full_messages
       }
     end
+  end
+
+  def destroy
+    user = User.find(id: params[:user_id])
+    mentor = user.mentors.find_by(mentor_id: params[:mentor_id])
+    reservation = Reservation.find_by(mentor_id: params[:mentor_id]).where(user_id: params[:user_id])
+    reservation.destroy
   end
 end
