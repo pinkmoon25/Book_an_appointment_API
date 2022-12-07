df9369abc5759148f04515a1b1a8a51630392281
Tushar <singhtushar348@gmail.com>
2022-12-07 19:28:07 +0530

    Create routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :sessions, only: [:create]
   resources :registrations, only: [:create]
-  resources :users
+  resources :users, only: [:show]
   delete :logout, to: "sessions#logout"
   get :logged_in, to: "sessions#logged_in"
   namespace :api do
