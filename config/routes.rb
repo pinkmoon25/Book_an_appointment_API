cbe8766dd83f55b15f775c31b5f28df7162c022f
Tushar <singhtushar348@gmail.com>
2022-12-07 15:04:56 +0530

    Add delete reservation route

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :mentors, only: [:index]
-      resources :reservations, only: [:create]
+      resources :reservations, only: [:create, :destroy]
     end
   end
 end
